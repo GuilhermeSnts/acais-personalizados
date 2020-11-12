6a0f0e8fc7df0c42be33387b0a7608ab7cb6b8ad
guilherme santos <guilhermess1994@gmail.com>
2020-11-12 03:35:47 -0300

    created select (initial implementation)

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,54 @@
+<template>
+  <select class="base-select" v-model="size">
+    <option value="" disabled selected>{{ placeholder }}</option>
+    <option :value="item" v-for="(item, index) in items" :key="index">{{
+      item
+    }}</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "BaseSelect",
+
+  computed: {
+    size: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        console.log(value);
+        this.$emit("input", value);
+      }
+    }
+  },
+
+  props: {
+    items: Array,
+    value: String,
+    placeholder: String
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+.base-select
+  border-radius: 25px
+  padding: 8px 15px
+  font-family: 'Roboto', sans-serif
+  border: 1px solid #52219d
+  appearance: none
+  outline: 0
+  cursor: pointer
+  flex: 1
+  color: #52219d
+  cursor: pointer
+  &:hover
+    color: #fff
+    background-color: #52219d
+    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1)
+    transition: all 0.3s ease 0s
+  option
+    background-color: #52219d
+    padding: 5px 10px
+</style>
