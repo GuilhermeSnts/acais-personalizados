7545355f7d01f5e0a7986a6e16a09d98f6ab42ff
guilherme santos <guilhermess1994@gmail.com>
2020-11-12 23:49:14 -0300

    created vuex action to clean checkout strore

--- src/store/checkout/actions.js
+++ src/store/checkout/actions.js
@@ -2,5 +2,12 @@ export default {
   DO_CHECKOUT(context, payload) {
     console.log(context, payload);
     return;
+  },
+  DO_CLEAN(context) {
+    context.commit("SET_SIZE", "");
+    context.commit("SET_FLAVOR", "");
+    context.commit("SET_CUSTOMIZATION", "");
+    context.commit("SET_PRICE", 0);
+    context.commit("SET_PREP", 0);
   }
 };
